c1b529ca848d56e5e2a10222affbeb8b750ec940
Monishat Baishnab <baishnabmonishat@gmail.com>
2024-06-18 23:41:15 +0600

    Add feature to validate slot overlap against existing slots.

--- dist/app/modules/slot/slot.services.js
+++ dist/app/modules/slot/slot.services.js
@@ -21,17 +21,27 @@ const fetchAvailableSlotFromDB = (query) => __awaiter(void 0, void 0, void 0, fu
 });
 const createSlotIntoDB = (slotData) => __awaiter(void 0, void 0, void 0, function* () {
     const serviceExists = yield service_model_1.default.findById(slotData.service);
+    const existingSlots = yield slot_model_1.default.find({ date: slotData === null || slotData === void 0 ? void 0 : slotData.date });
     if (!serviceExists) {
         throw new Error('Service not found.');
     }
+    existingSlots.forEach(element => {
+        const existingStartTime = new Date(`1970-01-01T${element.startTime}`);
+        const slotStartTime = new Date(`1970-01-01T${slotData.startTime}`);
+        const existingEndTime = new Date(`1970-01-01T${element.endTime}`);
+        const slotEndTime = new Date(`1970-01-01T${slotData.endTime}`);
+        if (existingStartTime < slotEndTime && existingEndTime > slotStartTime) {
+            throw new Error('Slot overlaps with an existing slot.');
+        }
+    });
     const slotsData = [];
     const timeSlots = (0, slot_utils_1.createTimeSlots)(slotData === null || slotData === void 0 ? void 0 : slotData.startTime, slotData === null || slotData === void 0 ? void 0 : slotData.endTime, serviceExists === null || serviceExists === void 0 ? void 0 : serviceExists.duration);
     timeSlots.forEach(({ startTime, endTime }) => {
         slotsData.push(Object.assign(Object.assign({}, slotData), { startTime,
             endTime }));
     });
-    const newSlot = yield slot_model_1.default.insertMany(slotsData);
-    return newSlot;
+    const newSlots = yield slot_model_1.default.insertMany(slotsData);
+    return newSlots;
 });
 exports.SlotServices = {
     fetchAvailableSlotFromDB,
--- src/app/modules/slot/slot.services.ts
+++ src/app/modules/slot/slot.services.ts
@@ -9,10 +9,22 @@ const fetchAvailableSlotFromDB = async (query: Record<string, unknown>) => {
 
 const createSlotIntoDB = async (slotData: TSlot) => {
     const serviceExists = await Service.findById(slotData.service);
+    const existingSlots = await Slot.find({date: slotData?.date});
 
     if (!serviceExists) {
         throw new Error('Service not found.');
     }
+
+    existingSlots.forEach(element => {
+        const existingStartTime = new Date(`1970-01-01T${element.startTime}`);
+        const slotStartTime = new Date(`1970-01-01T${slotData.startTime}`);
+        const existingEndTime = new Date(`1970-01-01T${element.endTime}`);
+        const slotEndTime = new Date(`1970-01-01T${slotData.endTime}`);
+
+        if(existingStartTime < slotEndTime && existingEndTime > slotStartTime){
+            throw new Error('Slot overlaps with an existing slot.');
+        }
+    });
 
     const slotsData : TSlot[] = [];
     const timeSlots = createTimeSlots(slotData?.startTime, slotData?.endTime, serviceExists?.duration);
@@ -25,9 +37,9 @@ const createSlotIntoDB = async (slotData: TSlot) => {
         })
     });
 
-    const newSlot = await Slot.insertMany(slotsData);
+    const newSlots = await Slot.insertMany(slotsData);
 
-    return newSlot;
+    return newSlots;
 };
 
 export const SlotServices = {
